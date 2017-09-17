96af54f2955b04f2c89f5f961d153f1bfa494fd0
nomanxd <32039794+nomanxd@users.noreply.github.com>
2017-09-17 23:59:20 +0300

    Update string_task.py

--- string_task.py
+++ string_task.py
@@ -12,5 +12,5 @@ def not_bad(s):
     bad = s.find("bad")
     nt = s.find("not")
     if nt > bad:
-        s = s.replace(s[nt: bad], "good")
+        s = s.replace(s[nt:(bad+3)], "good")
     return s
