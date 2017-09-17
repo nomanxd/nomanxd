e71cdeea14ddf0c2bc435938b8e6142fecb409cc
nomanxd <32039794+nomanxd@users.noreply.github.com>
2017-09-17 23:01:45 +0300

    Update string_task.py

--- string_task.py
+++ string_task.py
@@ -1,15 +1,15 @@
 def verbing(s):
-	if len(s) >= 3:
-		if s[-3:1] == ing:
-			s = s + "ly";
-		else:
-			s = s + "ing";
-	return s;
+    if len(s) >= 3:
+        if s[-3:1] == ing:
+            s = s + "ly";
+        else:
+            s = s + "ing";
+    return s;
 
 
 def not_bad(s):
-	bad = s.find("bad");
-	not = s.find("not");
-	if not > bad:
-		s = s.replace(s[not]:s[bad]:1);
-	return s;
+    bad = s.find("bad");
+    nt = s.find("not");
+    if nt > bad:
+        s = s.replace(s[nt]:s[bad]:1);
+    return s;
