3fc3858dd336549bfe725769de828d06203f2d43
nomanxd <32039794+nomanxd@users.noreply.github.com>
2017-09-17 21:56:57 +0300

    Add files via upload

---
+++ list_task.py
@@ -0,0 +1,16 @@
+def remove_adjacent(lst):
+	for i in range(len(lst)):
+		if lst[i] == lst[i-1]:
+	    		lst[i].pop;
+			i -= i;
+		i += i;
+	return lst;
+
+
+
+def linear_merge(lst1, lst2):
+	return sorted(lst1, lst2);
+
+
+
+
