22b9ee9858c87944b02c3d2434ed5d68d983d8fa
nomanxd <32039794+nomanxd@users.noreply.github.com>
2017-09-20 00:11:38 +0300

    Update string_task.py

--- string_task.py
+++ string_task.py
@@ -9,8 +9,9 @@ def verbing(s):
 
 
 def not_bad(s):
-    bad = s.find("bad") + 3
+    bad = s.find("bad")
     nt = s.find("not")
-    if nt > bad:
-        s = s.replace(s[nt:bad:], "good")
+    if nt < bad:
+        s = s.replace(s[nt: (bad+3):], 'good')
     return s
+
