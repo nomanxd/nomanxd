4d1f1be6dc7b87c78fd48ddfeeb168b6f85567b4
nomanxd <32039794+nomanxd@users.noreply.github.com>
2017-09-25 00:27:06 +0300

    Update find_duplikates.py

--- find_duplikates.py
+++ find_duplikates.py
@@ -6,11 +6,11 @@ from os import walk
 
 def sha1(path, blocksize = 65536):
     with open(filename, 'rb') as afile:
-    h = hashlib.sha1()
-    buf = afile.read(blocksize)
-    while len(buf) > 0:
-        h.update(buf)
+        h = hashlib.sha1()
         buf = afile.read(blocksize)
+        while len(buf) > 0:
+            h.update(buf)
+            buf = afile.read(blocksize)
     return h.hexdigest()
     
     
@@ -31,5 +31,4 @@ def findDup(direct):
     
     
 if __name__ == '__main__':
-    findDub(sys.argv[1])
-    
+    findDup(sys.argv[1])
