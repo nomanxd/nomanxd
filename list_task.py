1b41403d8dbccf963f8aaa1586ae979bde8e51e3
nomanxd <32039794+nomanxd@users.noreply.github.com>
2017-09-24 22:19:57 +0300

    Update list_task.py

--- list_task.py
+++ list_task.py
@@ -3,18 +3,14 @@ def remove_adjacent(lst):
     
 
 
-def linear_merge(lst1, lst2):
-    res = []
-    i = 0
-    j = 0
-    while ((i < len(lst1)) and (j < len(lst2))):
-        if lst1[i] < lst2[j]:
-            res.append(lst1[i])
-            i += 1
-        else:
-            res.append(lst2[j])
-            j += 1
-    return res
+ def linear_merge(lst1, lst2):
+      result = []
+      while len(lst1) and len(lst2):
+        if lst1[0] < lst2[0]:
+          result.append(lst1.pop(0))
+         else:
+          result.append(lst2.pop(0))
+      return result
         
 
 
